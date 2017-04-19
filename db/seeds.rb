7236917c47a2edfc488370403823f3ceecb34d8b
Jorge Anderson <jorgeandersonm@gmail.com>
2017-04-18 23:54:17 -0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+(1..10).each do
+	Individual.create!(
+		email: Faker::Internet.email,
+		name: Faker::Name.name,
+		address: Faker::Address.street_address,
+		city: Faker::Address.city,
+		gender: 'male',
+		age: Faker::Number.between(18, 67),
+		auditor: false,
+		password: '123123'
+	)
+end
+
+Individual.all.each do |individual|
+	(1..5).each do 
+		individual.goods.create!(
+			name: Faker::Commerce.product_name,
+    	category: Faker::Commerce.department(2, true),
+    	value: Faker::Commerce.price
+    )
+	end
+end
+
